a9e25f4ebd46df76093bf0cedf40dd477db6aa80
astianmuchui <sebastianmuchui79@gmail.com>
2022-10-31 19:25:18 +0300

    started

---
+++ src/booth/defines.h
@@ -0,0 +1,11 @@
+#define pump  2// Pump Relay
+#define servoL 12
+#define servoR 13
+#define echoPin 20 
+#define triggerPin 21
+#define entryServo 22
+#define exitServo 36
+#define pir 7
+#define IR1 8
+#define IR2 9
+#define buzzer 19
